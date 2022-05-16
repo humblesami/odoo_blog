f7ed102b167bfbb976ef18c489a62751eeb17fe6
sami92news <sami.akram@92newshd.tv>
2022-05-16 15:51:11 +0500

    blogs list

--- news/static/src/js/news_debug.js
+++ news/static/src/js/news_debug.js
@@ -1,10 +1,22 @@
 (function(){
-    let el = $('#o_wblog_post_top .bg_image_div').first();
-    let width = el.width();
+    let els = $('#o_wblog_post_top .bg_image_div');
+    if(!els.length){
+        els = $('article.o_wblog_post .bg_image_div');
+    }
+    if(!els.length){
+        return;
+    }
+    let first_el = els.first();
+    let width = first_el.width();
     let height = width * 0.5874;
-    el.height(height+'px');
-    el.next().remove();
-    el.next().remove();
-    el.removeClass('o_record_cover_component');
-    el.parent().removeClass('o_half_screen_height').removeClass('o_full_screen_height');
+    let height_to_apply = height+'px';
+
+    els.each(function(i, el){
+        el = $(el);
+        el.height(height_to_apply);
+        el.next().remove();
+        el.next().remove();
+        el.removeClass('o_record_cover_component');
+        el.parent().removeClass('o_half_screen_height').removeClass('o_full_screen_height');
+    });
 })();
