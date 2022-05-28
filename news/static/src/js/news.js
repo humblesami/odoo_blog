91eb79a42bf6ca0e4db2ccfe06b9151afd8123fc
sami92news <sami.akram@92newshd.tv>
2022-05-28 15:10:40 +0500

    news_debug

--- news/static/src/js/news.js
+++ news/static/src/js/news.js
@@ -1,6 +1,6 @@
 (function(){
     let dt = new Date();
-    let v = get.getMonth() + '-' + get.getDate() + '-' + dt.getHours() + '-' + dt.getMinutes() + '-' +  dt.getSeconds();
+    let v = dt.getMonth() + '-' + dt.getDate() + '-' + dt.getHours() + '-' + dt.getMinutes() + '-' +  dt.getSeconds();
     //$('body').append('<link rel="stylesheet" href="/news/static/src/css/news.css"/>');
     $('body').append('<script type="text/javascript" src="/news/static/src/js/news_debug.js?v='+v+'"></script>');
 })();
