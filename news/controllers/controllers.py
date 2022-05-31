3e646455cb01543b0e07101bd2fb81e25e36a9b4
sami92news <sami.akram@92newshd.tv>
2022-05-31 18:03:09 +0500

    set image heights

--- news/controllers/controllers.py
+++ news/controllers/controllers.py
@@ -1,33 +1,28 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
 # from odoo.http import request
-# from odoo.addons.website.controllers.main import Website
-# from odoo.addons.website.controllers.main import QueryURL
-# from odoo.addons.website_blog.controllers.main import WebsiteBlog
+# from odoo.addons.website.models.ir_qweb import QWeb
 #
 #
-# class NewsWebsite(Website):
+# class NewsQWeb(QWeb):
+#     """ QWeb object for rendering stuff in the website context """
 #
-#     @http.route('/', type='http', auth="public", website=True, sitemap=True)
-#     def index(self, **kw):
-#         homepage = request.website.homepage_id
-#         if homepage and (homepage.sudo().is_visible or request.env.user.has_group('base.group_user')) and homepage.url != '/':
-#             return request.env['ir.http'].reroute(homepage.url)
-#         website_page = request.env['ir.http']._serve_page()
-#         if website_page:
-#             blog_values = self.get_blog_values()
-#             homepage_posts = blog_values.get('posts') or []
-#             website_page.qcontext['posts'] = homepage_posts
-#             website_page.qcontext['len_posts'] = len(homepage_posts)
-#             return website_page
+#     _inherit = 'ir.qweb'
 #
-#         raise request.not_found()
-#
-#     def get_blog_values(self):
-#         opt = {}
-#         Blog = request.env['blog.blog']
-#         blogs = Blog.search(request.website.website_domain(), order="create_date asc, id asc")
-#         obj = WebsiteBlog()
-#         values = obj._prepare_blog_values(blogs=blogs, page=1)
-#         values['blog_url'] = QueryURL('/blog', ['tag'])
-#         return values
+#     def _post_processing_att(self, tagName, atts, options):
+#         is_blog_post = False
+#         if tagName == 'img' and 'loading' not in atts:
+#             src = atts['src']
+#             if src.startswith('/website_blog/static/src/img/cover'):
+#                 path = request.httprequest.path
+#                 if path.startswith('/blog/'):
+#                     try:
+#                         last_minus_index = path.rindex('-')
+#                         if last_minus_index != -1:
+#                             post_id = path[last_minus_index + 1:]
+#                             if post_id.isnumeric():
+#                                 is_blog_post = True
+#                     except:
+#                         pass
+#         atts = super(NewsQWeb, self)._post_processing_att(tagName, atts, options)
+#         if is_blog_post and atts.get('loading'):
+#             del atts['loading']
+#         return atts
