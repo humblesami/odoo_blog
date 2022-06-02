bc2a30f8a7bcc167a1750cd703bfe7d7e4d30ca9
sami92news <sami.akram@92newshd.tv>
2022-06-02 20:06:24 +0500

    wait_jquery

--- news/static/src/js/dynamic_blog_images.js
+++ news/static/src/js/dynamic_blog_images.js
@@ -15,7 +15,7 @@
 
         let els = document.querySelectorAll('.bg_image_div:not(.adjusted)');
         for(let el of els){
-            let height = el.clientWidth * 0.5874;
+            let height = el.clientWidth * 0.587;
             let height_to_apply = height + 'px';
             el.style.height = height_to_apply;
             el.classList.add("adjusted");
