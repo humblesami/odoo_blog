6fa0f5ae9b9086131aa7edb52e6d0e7f0c24a638
sami92news <sami.akram@92newshd.tv>
2022-06-02 12:38:59 +0500

    home page with custom data

--- news/static/src/js/news.js
+++ news/static/src/js/news.js
@@ -2,5 +2,5 @@
     let dt = new Date();
     let v = dt.getMonth() + '-' + dt.getDate() + '-' + dt.getHours() + '-' + dt.getMinutes() + '-' +  dt.getSeconds();
     //$('body').append('<link rel="stylesheet" href="/news/static/src/css/news.css"/>');
-    $('body').append('<script type="text/javascript" src="/news/static/src/js/news_debug.js?v='+v+'"></script>');
+    $('body').append('<script type="text/javascript" src="/news/static/src/js/news_debug.js?v=1"></script>');
 })();
