de5425dd4aba2aa3ccce870e0244873d47efe8b0
sami92news <sami.akram@92newshd.tv>
2022-06-03 18:21:35 +0500

    can purge cache

--- odoo_base/web_controller.py
+++ odoo_base/web_controller.py
@@ -1,3 +1,7 @@
+import json
+
+import requests
+
 from odoo import http
 from odoo.http import request
 
@@ -7,3 +11,36 @@ class Website(http.Controller):
     def get_user_id(self):
         user_id = request.uid or 'none'
         return str(user_id)
+
+    @http.route(['/purge/cache'], type='http')
+    def purge_cache(self):
+        res = {'status': 'error', 'message': 'Nothing'}
+        try:
+            url_to_purge = request.httprequest.values.get('url')
+            url_to_purge = 'http://voiceofnews.com/blog'
+            if not url_to_purge:
+                return 'Invalid Url'
+            token = 'hbWBy99DnEsV7ItOJMh40ssg6Ka_IOCZiEDCNMV-'
+            authorization = 'Bearer '+token
+            headers = {
+                'Authorization': authorization,
+                'Content-Type': 'application/json'
+            }
+            api_url = 'https://api.cloudflare.com/client/v4/zones/ae6d65e2cd285e9f741a60f301a4bfe8/purge_cache'
+            urls_to_purge = [
+                {'url': url_to_purge}
+            ]
+            json_data = {'files': urls_to_purge}
+            if url_to_purge == 'all':
+                json_data = {'purge_everything': True}
+            res = requests.post(api_url, headers=headers, json=json_data)
+            status = res.status_code
+            if status != 200:
+                res = res.json()
+            else:
+                res = {'status': 'success', 'urls': urls_to_purge }
+        except Exception as e:
+            res['message'] = 'Error in purge => ' + str(e)
+        res = json.dumps(res)
+        return res
+
--- odoo_base/web_models.py
+++ odoo_base/web_models.py
@@ -18,8 +18,7 @@ class CustomIrQWeb(IrQWeb):
     def _generate_asset_nodes(self, bundle, css=True, js=True, debug=False, async_load=False, defer_load=False, lazy_load=False, media=None):
         res = super()._generate_asset_nodes(bundle, css, js, debug, async_load, defer_load, lazy_load, media)
         if css:
-            if bundle == 'web.assets_common' or bundle == 'web.assets_frontend':
-                res[0][1]['rel'] = "preload"
-                res[0][1]['as'] = "style"
-                res[0][1]['onload'] = "this.rel='stylesheet'"
+            res[0][1]['rel'] = "preload"
+            res[0][1]['as'] = "style"
+            res[0][1]['onload'] = "this.rel='stylesheet'"
         return res
