b3bbc370fcf6e25c0b24e2f9198679fc75c686b3
sami92news <sami.akram@92newshd.tv>
2022-06-13 15:18:41 +0500

    no cache from odoo

--- odoo_base/cache.py
+++ odoo_base/cache.py
@@ -1,31 +1,37 @@
-import werkzeug
-
 import odoo
-from odoo.http import root
+import werkzeug
+from odoo.http import root, DisableCacheMiddleware  # , Response, HttpRequest
 
 
-def __call__(self, environ, start_response):
-    def start_wrapped(status, headers):
-        req = werkzeug.wrappers.Request(environ)
-        root.setup_session(req)
-        if req.session and req.session.debug and not 'wkhtmltopdf' in req.headers.get('User-Agent'):
-            if "assets" in req.session.debug and (".js" in req.base_url or ".css" in req.base_url):
-                new_headers = [('Cache-Control', 'no-store')]
-            else:
-                new_headers = []
-            for k, v in headers:
-                if k.lower() != 'cache-control':
-                    new_headers.append((k, v))
-            start_response(status, new_headers)
-        else:
+# class NewsHttpRequest(HttpRequest):
+#     def make_response(self, data, headers=None, cookies=None):
+#         found_at = -1
+#         for header in headers:
+#             found_at += 1
+#             if header[0].lower() == 'cache-control':
+#                 headers[found_at] = ('Cache-Control', 'public, max-age=7200, stale-while-revalidate=14400')
+#                 break
+#         response = Response(data, headers=headers)
+#         if cookies:
+#             for k, v in cookies.items():
+#                 response.set_cookie(k, v)
+#         return response
+
+
+class NewsDisableCacheMiddleware(DisableCacheMiddleware):
+    def __call__(self, environ, start_response):
+        def start_wrapped(status, headers):
+            req = werkzeug.wrappers.Request(environ)
+            root.setup_session(req)
             found_at = -1
             for header in headers:
                 found_at += 1
                 if header[0].lower() == 'cache-control':
-                    headers[found_at] = ('cache-from-origin', 'yes')
+                    headers[found_at] = ('cache-head-gone', 'yes')
                     break
             start_response(status, headers)
-    return self.app(environ, start_wrapped)
+        return self.app(environ, start_wrapped)
 
 
-odoo.http.DisableCacheMiddleware.__call__ = __call__
+odoo.http.DisableCacheMiddleware.__call__ = NewsDisableCacheMiddleware.__call__
+# odoo.http.HttpRequest.make_response = NewsHttpRequest.make_response
