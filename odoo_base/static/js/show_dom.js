3ca24ccee7e906c4edcf2028ca7d79abe8e7c756
sami92news <sami.akram@92newshd.tv>
2022-06-14 17:34:09 +0500

    with lazy loads

--- odoo_base/static/js/show_dom.js
+++ odoo_base/static/js/show_dom.js
@@ -1,18 +1,14 @@
 (function () {
 
-//    let links = document.querySelectorAll('link[type="text/css"]');
-//    console.log(links.length + ' css files');
-//    for(let link of links){
-//        link.onload = function(){
-//            console.log(link.href);
-//        }
-//    }
-
     let if_css_failed = setTimeout(function(){
         let message = 'Failed loading css';
+        if(window.css_waiter.afe_loaded){
+            message = 'Front end assets loaded';
+            show_dom(message);
+            return;
+        }
         show_dom(message);
-        console.log(message);
-    }, 25000);
+    }, 3000);
 
     function show_dom(args) {
         let main_loader = document.getElementById('css_waiter_dom');
--- odoo_base/static/js/top_script.js
+++ odoo_base/static/js/top_script.js
@@ -1,14 +1,19 @@
 (function () {
     window.css_waiter = {
-        css_loaded: 0,
+        css_loaded : 0,
+        afe_loaded : 0,
         waiting_functions: [],
         on_file_loaded: function (link_el) {
             let waiter_obj = window.css_waiter;
             link_el.onload = undefined;
             let href = link_el.href;
             console.log(href);
-            if(href.endsWith('/web.assets_frontend.min.css')){
-                waiter_obj.css_loaded = 1;
+            if(href.endsWith('/web.assets_frontend.min.css') || href.endsWith('/assets_common.min.css')){
+                if(href.endsWith('/web.assets_frontend.min.css'))
+                {
+                    css_waiter.afe_loaded = 1;
+                }
+                waiter_obj.css_loaded += 1;
                 for (let fun of waiter_obj.waiting_functions) {
                     fun();
                 }
@@ -17,7 +22,7 @@
         },
         wait_or_execute: function (waiter_function) {
             let waiter_obj = window.css_waiter;
-            if (waiter_obj.css_loaded) {
+            if (waiter_obj.css_loaded == 2) {
                 waiter_function();
             }
             else {
