bcbe030e55b462e0cfa3b8eeb3bd040cbe4f9da1
sami92news <sami.akram@92newshd.tv>
2022-06-15 13:24:27 +0500

    without font awesome

--- odoo_base/static/js/show_dom.js
+++ odoo_base/static/js/show_dom.js
@@ -34,7 +34,14 @@
     function set_image_heights(){
         els = document.querySelectorAll('.bg_image_div:not(.adjusted)');
         for(let el of els){
-            let height = el.clientWidth * 0.587;
+            let el_width = $(el).width();
+            if(!el_width){
+                el_width = $(el).parent().width();
+            }
+            if(!el_width){
+                console.log(el.parentNode, 'has no width');
+            }
+            let height = el_width * 0.587;
             let height_to_apply = height + 'px';
             el.style.height = height_to_apply;
             el.classList.add("adjusted");
