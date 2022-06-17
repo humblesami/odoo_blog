0aab49dc97ccefa8e350200a01d4506620b87d05
sami92news <sami.akram@92newshd.tv>
2022-06-17 13:49:50 +0500

    jquery at top

--- odoo_base/static/js/show_dom.js
+++ odoo_base/static/js/show_dom.js
@@ -42,7 +42,8 @@
             let height = el_width * 0.587;
             let height_to_apply = height + 'px';
             console.log(el, height_to_apply);
-            $(el).find('img.img_cover_image').css({'width': '100%', 'height': height_to_apply});
+            el.childNode.width = '100%';
+            el.childNode.height = height_to_apply;
             el.classList.add("adjusted");
             if(el.nextElementSibling)
             {
@@ -95,5 +96,5 @@
     {
         on_css_wait_time_out();
     }
-    console.log('show dom v2')
+    console.log('show dom v3')
 })()
