3e3f84fbdaeaf6f3bc4261b5609682ce33a41b1a
sami92news <sami.akram@92newshd.tv>
2022-06-17 15:07:18 +0500

    jquery at top

--- odoo_base/__manifest__.py
+++ odoo_base/__manifest__.py
@@ -22,10 +22,7 @@
     # any module necessary for this one to work correctly
     'depends': ['base','web'],
     'assets': {
-        'web.assets_common_minimal': [
-            'web/static/lib/jquery/jquery.js',
-            'odoo_base/static/js/show_dom.js'
-        ],
+        'web.assets_common_minimal': [],
         'web.assets_frontend': [
             ('replace', 'website/static/src/scss/website.scss', 'odoo_base/static/css/website.scss'),
             'odoo_base/static/js/purger.js'
