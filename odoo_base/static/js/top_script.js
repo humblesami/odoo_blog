d22fd71d28a0719cc85d0e6da108711c277e02b5
sami92news <sami.akram@92newshd.tv>
2022-06-20 15:28:02 +0500

    unalod event

--- odoo_base/static/js/top_script.js
+++ odoo_base/static/js/top_script.js
@@ -1,4 +1,4 @@
 (function(){
-    window.unbind("unload");document.unbind("unload");window.off("unload");document.off("unload");window.on("load", function(){});$(document).on("load", function(){});console.log('Remove unload and add load added');
+    $(window).unbind("unload");$(document).unbind("unload");$(window).off("unload");$(document).off("unload");$(window).on("load", function(){});$(document).on("load", function(){});console.log('Remove unload and add load added');
     window.on_front_end=1;window.css_waiter={css_loaded:0,afe_loaded:0,waiting_functions:[],on_file_loaded:function(i){let n=window.css_waiter;if(i.onload=void 0,i.href.endsWith("/web.assets_frontend.min.css")){css_waiter.afe_loaded=1;for(let i of n.waiting_functions)i();n.waiting_functions=[]}},wait_or_execute:function(i){let n=window.css_waiter;n.afe_loaded?i():n.waiting_functions.push(i)}};
 })();
